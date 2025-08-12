7ae1a01de1c302d3285f6829c6e72d70a57a82f1
mhdirfanramadhan <akunml12ku@gmail.com>
2025-08-12 14:35:57 +0700

    add menu tujuan kapal

--- lib/validations/tujuankapal.validation.ts
+++ lib/validations/tujuankapal.validation.ts
@@ -2,11 +2,13 @@ import { REQUIRED_FIELD } from '@/constants/validation';
 import { z } from 'zod';
 import { dynamicRequiredMessage } from '../utils';
 
-export const containerSchema = z.object({
+export const tujuankapalSchema = z.object({
   nama: z.string().nonempty({ message: dynamicRequiredMessage('NAMA') }), // Nama wajib diisi
   keterangan: z.string().nullable().optional(), // Keterangan opsional
+  cabang_id: z.number().min(1, `${REQUIRED_FIELD}`), // Status aktif wajib diisi
+  namacabang: z.string().nullable().optional(),
   statusaktif: z.number().min(1, `${REQUIRED_FIELD}`), // Status aktif wajib diisi
   statusaktif_nama: z.string().nullable().optional() // Status aktif wajib diisi
 });
 
-export type ContainerInput = z.infer<typeof containerSchema>;
+export type TujuankapalInput = z.infer<typeof tujuankapalSchema>;
