7b528eec3d753826cf7403bfbfe92b22a2ee3d8c
Ryanita1402 <ryan_vixy1402@yahoo.com>
2025-08-14 16:15:10 +0700

    penambahan daftar bank

---
+++ lib/validations/daftarbank.validation.ts
@@ -0,0 +1,11 @@
+import { REQUIRED_FIELD } from '@/constants/validation';
+import { z } from 'zod';
+import { dynamicRequiredMessage } from '../utils';
+
+export const daftarbankSchema = z.object({
+  nama: z.string().nonempty({ message: dynamicRequiredMessage('NAMA') }), // Nama wajib diisi
+  keterangan: z.string().nullable().optional(), // Keterangan opsional
+  statusaktif: z.number().min(1, `${REQUIRED_FIELD}`), // Status aktif wajib diisi
+  statusaktif_nama: z.string().nullable().optional() // Email wajib diisi
+});
+export type DaftarBankInput = z.infer<typeof daftarbankSchema>;
