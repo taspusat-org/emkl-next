2e83e70f0e1f3eb8b9473405efbb0d1004e47f4b
mhdirfanramadhan <akunml12ku@gmail.com>
2025-10-09 16:25:13 +0700

    change base api

--- lib/apis/print.api.ts
+++ lib/apis/print.api.ts
@@ -24,7 +24,7 @@ export interface PrintFileBody {
   options: PrintOptions;
 }
 
-const BASE = '/api/bridge/printer';
+const BASE = 'http://localhost:3004/api/printer';
 
 export async function getPrintersFn(): Promise<PrinterInfo[]> {
   try {
